9a64adb5c49ebdc49b521b5688af7783144c23bc
Alexander Zakharov <alexander.zakharov@capgemini.com>
2020-02-13 23:13:54 +0100

    chore: add TypeScript support to jest

--- jest.config.js
+++ jest.config.js
@@ -1,25 +1,18 @@
 module.exports = {
-  testMatch: ['<rootDir>/test/**/(test).js?(x)', '**/?(*.)(test).js?(x)'],
-  testPathIgnorePatterns: [
-    '/node_modules/',
-    '<rootDir>/test/jestsetup.js',
-    '<rootDir>/test/fixtures/',
-    '/target/'
-  ],
-  setupFiles: ['<rootDir>/test/jestsetup.js'],
+  preset: 'ts-jest/presets/js-with-babel',
+  setupFiles: ['<rootDir>/test/setup.js'],
   transform: {
-    '^.+\\.(js|jsx)$': 'babel-jest',
     '^.+\\.svg$': 'jest-svg-transformer'
   },
   snapshotSerializers: ['enzyme-to-json/serializer'],
   moduleNameMapper: {
     '^.+\\.(css|less|scss)$': 'identity-obj-proxy'
   },
-  collectCoverage: false,
+  coverageDirectory: 'coverage',
   collectCoverageFrom: [
-    'src/**/*.{js,jsx}',
-    '!src/redux/store/**/*.{js,jsx}',
-    '!src/redux/middleware/**/*.{js,jsx}'
-  ],
-  coverageDirectory: 'coverage'
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/**/*.d.ts',
+    '!dist',
+    '!coverage'
+  ]
 };
