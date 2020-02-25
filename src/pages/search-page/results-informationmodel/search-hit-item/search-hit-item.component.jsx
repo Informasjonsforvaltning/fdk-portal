7e24e9e1e6ae041f14a2469f0a69e605daa36a23
Øyvind Isaksen <oyvindisaks1@gmail.com>
2020-02-25 10:07:11 +0100

    chore: update lint rules and fix linting

--- src/pages/search-page/results-informationmodel/search-hit-item/search-hit-item.component.jsx
+++ src/pages/search-page/results-informationmodel/search-hit-item/search-hit-item.component.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
-import _ from 'lodash';
 
 import localization from '../../../../lib/localization';
 import './search-hit-item.scss';
