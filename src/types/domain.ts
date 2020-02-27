c050f3800e787d230a7943949d238527a194ef85
Øyvind Isaksen <oyvindisaks1@gmail.com>
2020-02-27 09:45:02 +0100

    chore: #3004 take 2 - change infomodel structure according to new spec

--- src/types/domain.ts
+++ src/types/domain.ts
@@ -4,62 +4,24 @@ export interface InformationModelDocument {
   harvestSourceUri?: string;
   harvest?: Partial<Harvest>;
   title?: Partial<TextLanguage>;
-  informationModelDescription?: Partial<ModelDescription>;
-  rootObject?: Partial<ObjectType>;
-  objectTypes?: Partial<ObjectType>[];
-  codeTypes?: Partial<CodeType>[];
-  dataTypes?: Partial<DataType>[];
-  simpleTypes?: SimpleType[];
   schema?: string;
+  objectTypes?: Partial<Node>[];
+  codeTypes?: Partial<Node>[];
+  dataTypes?: Partial<Node>[];
+  simpleTypes?: Partial<Node>[];
+  description?: Partial<TextLanguage>;
 }
 
-export interface ObjectType {
+export interface Node {
   id: string;
-  modelDescription: Partial<ModelDescription>;
-  attributeList: Partial<ModelElement>[];
-  roleList: Partial<ModelElement>[];
-  extendsFrom: ObjectType[];
-}
-
-export interface CodeType {
-  id: string;
-  modelDescription: ModelDescription;
-  codeList: ModelElement[];
-  externalCodeList: string[];
-}
-
-export interface DataType {
-  id: string;
-  modelDescription: Partial<ModelDescription>;
-  attributeList: Partial<ModelElement>[];
-  extendsFrom: ObjectType[];
-}
-
-export interface SimpleType {
-  id: string;
-  modelDescription: ModelDescription;
-  extendsFrom: ObjectType[];
-  distributionReference: string[];
-}
-
-export interface ModelElement {
   name: Partial<TextLanguage>;
-  referencedObject: Partial<ReferencedObject>;
-  range: string;
-  elementDescription: Partial<ModelDescription>;
-}
-
-export interface ModelDescription {
-  name: Partial<TextLanguage>;
-  description: Partial<TextLanguage>;
-  identifier: string;
-  belongsToModule?: string;
-  concept: Partial<Concept>;
-}
-
-export interface ReferencedObject {
-  refId: string;
-  name: Partial<TextLanguage>;
+  roles: Partial<Property>[];
+  attributes: Partial<Property>[];
+  properties: Partial<Property>[];
+  isSubClassOf: string;
+  modelElementType: string;
+  isDescribedByUrl: string;
+  typeDefinitionReference: string;
 }
 
 export interface TextLanguage {
@@ -81,9 +43,11 @@ export interface Harvest {
   lastHarvested: string;
 }
 
-export interface Concept {
-  anbefaltTerm: Partial<TextLanguage>;
-  definition: Partial<TextLanguage>;
-  publisher: Partial<Publisher>;
-  conceptReference: string;
+export interface LosTheme {
+  uri: string;
+}
+
+export interface Property {
+  name: Partial<TextLanguage>;
+  parameters: any;
 }
