f74982c28237c65b97940a601f2a409c373b08ca
Alexander Zakharov <alexander.zakharov@capgemini.com>
2020-03-04 14:56:15 +0100

    fix: fetch information model references by concept identifier

--- src/pages/concept-details-page/resolved-concept-details-page.jsx
+++ src/pages/concept-details-page/resolved-concept-details-page.jsx
@@ -3,10 +3,6 @@ import { resolve } from 'react-resolver';
 import { ConceptDetailsPage } from './concept-details-page';
 import { getConcept } from '../../api/concepts';
 import { datasetsSearch, extractDatasets } from '../../api/datasets';
-import {
-  informationmodelsSearch,
-  extractInformationmodels
-} from '../../api/informationmodels';
 
 const getDatasetsForConcept = id => {
   const subject = `https://fellesdatakatalog.brreg.no/api/concepts/${id}`;
@@ -14,21 +10,12 @@ const getDatasetsForConcept = id => {
   return datasetsSearch({ subject, returnfields }).then(extractDatasets);
 };
 
-const getInformationModelsForConcept = id =>
-  informationmodelsSearch({
-    conceptUris: `https://fellesdatakatalog.brreg.no/api/concepts/${id}`,
-    returnfields: ['id', 'title'].join()
-  }).then(extractInformationmodels);
-
 const memoizedGetConcept = _.memoize(getConcept);
 const memoizedGetDatasets = _.memoize(getDatasetsForConcept);
-const memoizedGetInformationModels = _.memoize(getInformationModelsForConcept);
 
 const mapProps = {
   conceptItem: props => memoizedGetConcept(props.match.params.id),
-  conceptDatasetReferences: props => memoizedGetDatasets(props.match.params.id),
-  conceptInformationModelReferences: props =>
-    memoizedGetInformationModels(props.match.params.id)
+  conceptDatasetReferences: props => memoizedGetDatasets(props.match.params.id)
 };
 
 export const ResolvedConceptDetailsPage = resolve(mapProps)(ConceptDetailsPage);
