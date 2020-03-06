60f08db50b17b1e9c5b075ac0f2da61dbab32bda
valosnah <exthos@brreg.no>
2020-03-06 10:45:02 +0100

    chore: changed redirect url to be set from env variable

--- src/pages/maintenance-page/index.tsx
+++ src/pages/maintenance-page/index.tsx
@@ -10,7 +10,7 @@ import SC from './styled';
 const MaintenancePage: FC = () => {
   useEffect(() => {
     setTimeout(
-      () => location.replace(getConfig().searchHost || 'https://fellesdatakatalog.digdir.no'),
+      () => location.replace(getConfig().searchHost.host || 'https://fellesdatakatalog.digdir.no'),
       10000
     );
   }, []);
@@ -33,7 +33,7 @@ const MaintenancePage: FC = () => {
           <p>Du finner oss her:</p>
         </SC.Subheading>
         <SC.Link
-          uri={getConfig().searchHost || 'https://fellesdatakatalog.digdir.no'}
+          uri={getConfig().searchHost.host || 'https://fellesdatakatalog.digdir.no'}
           prefLabel="fellesdatakatalog.digdir.no"
         />
         <SC.RedirectionNotice>
