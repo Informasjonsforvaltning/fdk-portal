d9bae5902f1b5e0aa5aaab08a0c3a3dda41883a8
Øyvind Isaksen <oyvindisaks1@gmail.com>
2020-03-16 10:24:52 +0100

    fix(informationmodel-item): filter on themes

--- src/pages/search-page/results-informationmodel/informationmodel-item/informationmodel-item.component.tsx
+++ src/pages/search-page/results-informationmodel/informationmodel-item/informationmodel-item.component.tsx
@@ -8,20 +8,33 @@ import {
 } from '../../../../components/search-hit/search-hit';
 import { getTranslateText } from '../../../../lib/translateText';
 import { RoundedTag } from '../../../../components/rounded-tag/rounded-tag.component';
+import { PATHNAME_INFORMATIONMODELS } from '../../../../constants/constants';
+import { patchSearchQuery } from '../../../../lib/addOrReplaceUrlParam';
 
 interface Props {
   informationModel: InformationModelDocument;
-  referenceData: any;
+  losItems: any;
 }
 
-const renderThemes = (themes: any) => {
+const renderThemes = (themes: any, losItems: any) => {
   return themes
     .map((theme: any, index: number) => {
-      const { name } = theme;
+      const losItem: any =
+        Object.values(losItems).find(
+          (losItem: any) => losItem.uri === theme.uri
+        ) || {};
+      const losPath: any = losItem?.losPaths ? losItem.losPaths[0] : '';
+      const prefLabel = getTranslateText(losItem.prefLabel);
       return (
-        name && (
-          <RoundedTag key={`dataset-theme-${index}`} to="">
-            <span>{getTranslateText(name)}</span>
+        prefLabel && (
+          <RoundedTag
+            key={`dataset-theme-${index}`}
+            to={`${PATHNAME_INFORMATIONMODELS}${patchSearchQuery(
+              'losTheme',
+              losPath
+            )}`}
+          >
+            <span>{prefLabel}</span>
           </RoundedTag>
         )
       );
@@ -30,7 +43,8 @@ const renderThemes = (themes: any) => {
 };
 
 export const InformationModelItem: FC<Props> = ({
-  informationModel: { id, title = {}, description, publisher = {}, themes }
+  informationModel: { id, title = {}, description, publisher = {}, themes },
+  losItems
 }) => {
   return (
     <SearchHit
@@ -40,7 +54,9 @@ export const InformationModelItem: FC<Props> = ({
       publisher={publisher}
       description={description}
     >
-      {themes && <SearchHitThemes>{renderThemes(themes)}</SearchHitThemes>}
+      {themes && (
+        <SearchHitThemes>{renderThemes(themes, losItems)}</SearchHitThemes>
+      )}
     </SearchHit>
   );
 };
