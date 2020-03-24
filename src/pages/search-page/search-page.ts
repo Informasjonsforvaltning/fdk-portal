4c3b88e66b563d7951355d2cc333f37d79759af7
Øyvind Isaksen <oyvindisaks1@gmail.com>
2020-03-24 15:22:39 +0100

    feat(search-page): #84 add new page search all entities

--- src/pages/search-page/search-page.ts
+++ src/pages/search-page/search-page.ts
@@ -2,7 +2,8 @@ import { compose } from 'redux';
 
 import SearchPagePure from './search-page-pure';
 import { searchPageConnector } from './search-page-connector';
+import { searchPageResolver } from './search-page-resolver';
 
-const enhance = compose(searchPageConnector);
+const enhance = compose(searchPageResolver, searchPageConnector);
 
 export const SearchPage = enhance(SearchPagePure);
