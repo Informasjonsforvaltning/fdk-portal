06b18950978523706110e8ae36558246205d1dad
Øyvind Isaksen <oyvindisaks1@gmail.com>
2020-03-24 19:19:37 +0100

    chore(search-page): change tabs layout and staging

--- src/pages/search-page/tabs/components/tabs/styled.ts
+++ src/pages/search-page/tabs/components/tabs/styled.ts
@@ -10,74 +10,81 @@ const Tabs = styled.ul`
   align-items: center;
   display: flex;
   justify-content: center;
-  flex-wrap: wrap;
+  flex-wrap: nowrap;
   margin-bottom: 0;
   padding-left: 0;
   @media (max-width: 768px) {
     justify-content: space-around;
-    margin: 0 10%;
   }
 `;
 
 const Label = styled.span`
   margin-left: 0.5em;
+  white-space: nowrap;
   @media (max-width: 768px) {
     display: none;
     margin-left: 0;
   }
 `;
 
-const MobileLabel = styled.div`
-  background-color: #eeeff0;
-  display: flex;
-  justify-content: center;
-  font-size: 2rem;
-  padding-top: 1em;
-  @media (min-width: 768px) {
-    display: none;
-  }
-`;
-
 const AllIcon = styled(AllIconBase)`
   height: 1.5em;
+  @media (min-width: 768px) {
+    height: 1em;
+  }
 `;
 
 const DatasetIcon = styled(DatasetIconBase)`
   border-radius: 50%;
-  padding: 0.4em;
+  padding: 0.5em;
   background-color: ${({ theme }) => theme.colors.dataset.light};
-  height: 2em;
+  height: 3em;
   fill: ${({ theme }) => theme.colors.dataset.dark};
+
+  @media (min-width: 768px) {
+    height: 2em;
+  }
 `;
 
 const ApiIcon = styled(ApiIconBase)`
   border-radius: 50%;
-  padding: 0.4em;
+  padding: 0.5em;
   background-color: ${({ theme }) => theme.colors.api.light};
-  height: 2em;
+  height: 3em;
   fill: ${({ theme }) => theme.colors.api.dark};
+
+  @media (min-width: 768px) {
+    height: 2em;
+  }
 `;
 
 const ConceptIcon = styled(ConceptIconBase)`
   border-radius: 50%;
-  padding: 0.4em;
+  padding: 0.5em;
   background-color: ${({ theme }) => theme.colors.concept.light};
-  height: 2em;
+  height: 3em;
   fill: ${({ theme }) => theme.colors.concept.dark};
+
+  @media (min-width: 768px) {
+    height: 2em;
+  }
 `;
 
 const InfomodIcon = styled(InfomodIconBase)`
   border-radius: 50%;
-  padding: 0.4em;
+  padding: 0.5em;
   background-color: ${({ theme }) => theme.colors.infomod.light};
-  height: 2em;
+  height: 3em;
   fill: ${({ theme }) => theme.colors.infomod.dark};
+
+  @media (min-width: 768px) {
+    height: 2em;
+  }
 `;
 
 export default {
   Tabs,
   Label,
-  MobileLabel,
   AllIcon,
   DatasetIcon,
   ApiIcon,
