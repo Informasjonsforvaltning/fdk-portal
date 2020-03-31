7060c3003aa87d69ed8934e733f66efa639e90bb
Øyvind Isaksen <oyvindisaks1@gmail.com>
2020-03-31 06:58:30 +0200

    chore: #175 add search-link component

--- src/components/search-box/components/search-box/search-box.component.tsx
+++ src/components/search-box/components/search-box/search-box.component.tsx
@@ -8,6 +8,7 @@ import React, {
 
 import SC from './styled';
 import SearchForm from '../search-form/search-form.component';
+import SearchLink from '../search-link/search-link.component';
 import SearchBoxTitle from '../search-box-title/search-box-title.component';
 import { Tabs } from '../../../../pages/search-page/tabs/tabs';
 
@@ -16,6 +17,11 @@ const SearchBox: FC<PropsWithChildren<any>> = ({ children }) => {
     Children.map(children, child =>
       isValidElement(child) && child.type === SearchBoxTitle ? child : null
     )?.shift();
+
+  const renderSearchLinks = () =>
+    Children.map(children, child =>
+      isValidElement(child) && child.type === SearchLink ? child : null
+    );
 
   const renderTabs = () =>
     Children.map(children, child =>
@@ -27,6 +33,9 @@ const SearchBox: FC<PropsWithChildren<any>> = ({ children }) => {
       <SC.Content>
         {renderSearchBoxTitle()}
         <SearchForm />
+        {Children.count(renderSearchLinks()) > 0 && (
+          <SC.SearchLinks>{renderSearchLinks()}</SC.SearchLinks>
+        )}
       </SC.Content>
       {renderTabs()}
     </SC.SearchBox>
---
+++ src/components/search-box/components/search-link/styled.ts
@@ -0,0 +1,108 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+import DatasetIconBase from '../../../../images/icon-catalog-dataset-lg.svg';
+import ApiIconBase from '../../../../images/icon-catalog-api-lg.svg';
+import ConceptIconBase from '../../../../images/icon-catalog-concept-lg.svg';
+import InfomodIconBase from '../../../../images/icon-catalog-infomod-lg.svg';
+import { Entity } from '../../../../types/enums';
+
+const SearchLink = styled(Link)<{ type?: Entity }>`
+  align-items: center;
+  background-color: ${({ type, theme }) =>
+    type ? theme.colors[type]?.light : '#FFF'};
+  border-radius: 5px;
+  color: ${({ theme }) => theme.colors.neutralDarker} !important;
+  display: flex;
+  font-size: 1.8rem;
+  margin-bottom: 1em;
+  padding: 1em;
+  text-decoration: none;
+  word-break: break-all;
+  transition: all 200ms ease-in-out;
+
+  svg {
+    path {
+      fill: ${({ type, theme }) =>
+        type ? theme.colors[type]?.dark : '#121619'};
+    }
+  }
+
+  span:last-child {
+    font-weight: 500;
+    margin-left: 0.2em;
+    text-align: center;
+  }
+
+  @media (min-width: 992px) {
+    flex-flow: column;
+    font-size: 1.6rem;
+    width: 23%;
+
+    span:last-child {
+      font-size: 1.8rem;
+    }
+  }
+
+  &:hover {
+    background-color: #121619;
+    color: #fff !important;
+    svg {
+      path {
+        fill: #fff;
+      }
+    }
+  }
+`;
+
+const DatasetIcon = styled(DatasetIconBase)`
+  height: 1.5em;
+  margin-right: 0.5em;
+
+  @media (min-width: 992px) {
+    height: 3em;
+    margin-right: 0;
+    margin-bottom: 0.5em;
+  }
+`;
+
+const ApiIcon = styled(ApiIconBase)`
+  height: 1.5em;
+  margin-right: 0.5em;
+
+  @media (min-width: 992px) {
+    height: 3em;
+    margin-right: 0;
+    margin-bottom: 0.5em;
+  }
+`;
+
+const ConceptIcon = styled(ConceptIconBase)`
+  height: 1.5em;
+  margin-right: 0.5em;
+
+  @media (min-width: 992px) {
+    height: 3em;
+    margin-right: 0;
+    margin-bottom: 0.5em;
+  }
+`;
+
+const InfomodIcon = styled(InfomodIconBase)`
+  height: 1.5em;
+  margin-right: 0.5em;
+
+  @media (min-width: 992px) {
+    height: 3em;
+    margin-right: 0;
+    margin-bottom: 0.5em;
+  }
+`;
+
+export default {
+  SearchLink,
+  DatasetIcon,
+  ApiIcon,
+  ConceptIcon,
+  InfomodIcon
+};
