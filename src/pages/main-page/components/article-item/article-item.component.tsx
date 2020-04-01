d046d05043cb1e4ded3d45d2ad4abd683a70aa91
Øyvind Isaksen <oyvindisaks1@gmail.com>
2020-04-01 20:12:01 +0200

    feat(news-list): add link to article

--- src/pages/main-page/components/article-item/article-item.component.tsx
+++ src/pages/main-page/components/article-item/article-item.component.tsx
@@ -1,15 +1,17 @@
 import React, { FC, memo } from 'react';
 import Moment from 'react-moment';
+import { Link } from 'react-router-dom';
 
 import SC from './styled';
 
 interface Props {
+  id: string;
   date?: string;
   title?: string;
   abstract?: string;
 }
 
-const ArticleItem: FC<Props> = ({ date, title, abstract }) => (
+const ArticleItem: FC<Props> = ({ id, date, title, abstract }) => (
   <SC.ArticleItem>
     <header>
       {date && (
@@ -17,7 +19,11 @@ const ArticleItem: FC<Props> = ({ date, title, abstract }) => (
           <Moment format="DD.MM.YYYY">{date}</Moment>
         </SC.Date>
       )}
-      {title && <SC.Title>{title}</SC.Title>}
+      {title && (
+        <SC.Title>
+          <Link to={`news/${id}`}>{title}</Link>
+        </SC.Title>
+      )}
     </header>
     {abstract && <SC.Abstract>{abstract}</SC.Abstract>}
   </SC.ArticleItem>
