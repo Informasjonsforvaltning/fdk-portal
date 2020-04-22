d0399c6dbb365b1afed6c8f44399e20e7571e613
Øyvind Isaksen <oyvindisaks1@gmail.com>
2020-04-22 14:11:48 +0200

    feat(news-article-page): #248 add published date, image and video

--- src/api/cms-api/news-entities.ts
+++ src/api/cms-api/news-entities.ts
@@ -8,7 +8,7 @@ export const getRecentNewsEntities = (pageLimit: number) =>
 
 export const getNewsEntity = (id: string) =>
   cmsApiGet(
-    `/node/news/${id}?include=field_modules,field_modules.field_image,field_modules.field_image.field_media_image,field_global_taxonomy`
+    `/node/news/${id}?include=field_modules,field_modules.field_image,field_modules.field_image.field_media_image,field_global_taxonomy,field_modules.field_remote_video`
   ).then(deserialize);
 
 export const extractNewsData = (response: any) => response.data ?? [];
