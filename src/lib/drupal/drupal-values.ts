171e79e5837f063fb905d1a0d17aa3dc0a1fe927
Øyvind Isaksen <oyvindisaks1@gmail.com>
2020-04-22 14:11:48 +0200

    feat(news-article-page): #248 add published date, image and video

--- src/lib/drupal/drupal-values.ts
+++ src/lib/drupal/drupal-values.ts
@@ -1,4 +1,14 @@
 const PARAGRAPH__BODY = 'paragraph--body';
+const PARAGRAPH__IMAGE = 'paragraph--image';
+const PARAGRAPH__VIDEO = 'paragraph--video';
 
 export const getParagraphBodyValue = (fields: any) =>
   fields?.find((item: any) => item.type === PARAGRAPH__BODY)?.field_body?.value;
+
+export const getParagraphImage = (fields: any) =>
+  fields?.find((item: any) => item.type === PARAGRAPH__IMAGE)?.field_image
+    ?.field_media_image;
+
+export const getParagraphVideoValue = (fields: any) =>
+  fields?.find((item: any) => item.type === PARAGRAPH__VIDEO)
+    ?.field_remote_video?.field_media_oembed_video;
--- src/pages/news-article-page/news-article-page-pure.tsx
+++ src/pages/news-article-page/news-article-page-pure.tsx
@@ -1,28 +1,47 @@
-import React, { FC, memo } from 'react';
+import React, { FC, memo, useEffect, useState } from 'react';
 
 import SC from './styled';
 import { News as NewsInterface } from '../../types';
-import { getParagraphBodyValue } from '../../lib/drupal/drupal-values';
+import {
+  getParagraphBodyValue,
+  getParagraphImage,
+  getParagraphVideoValue
+} from '../../lib/drupal/drupal-values';
 import Article from '../../components/article/components/article/article.component';
+import { getVimeoData } from '../../api/vimeo/host';
 
 interface Props {
   newsItem?: Partial<NewsInterface>;
 }
 
 const NewsArticlePage: FC<Props> = ({
-  newsItem: { title, field_ingress, field_modules } = {}
-}) => (
-  <main id="content" className="container">
-    <SC.Content className="row">
-      <div className="col-12 col-lg-8 offset-lg-2">
-        <Article
-          title={title}
-          abstract={field_ingress}
-          body={getParagraphBodyValue(field_modules)}
-        />
-      </div>
-    </SC.Content>
-  </main>
-);
+  newsItem: { created, title, field_ingress, field_modules } = {}
+}) => {
+  const videoLinkValue = getParagraphVideoValue(field_modules);
+  const [vimeoData, setVimeoData] = useState();
+
+  useEffect(() => {
+    if (videoLinkValue) {
+      getVimeoData(videoLinkValue).then(setVimeoData);
+    }
+  }, []);
+
+  return (
+    <main id="content" className="container">
+      <SC.Content className="row">
+        <div className="col-12 col-lg-8 offset-lg-2">
+          <Article
+            publishedDate={created}
+            title={title}
+            abstract={field_ingress}
+            body={getParagraphBodyValue(field_modules)}
+            imageTop={getParagraphImage(field_modules)}
+            vimeoData={vimeoData}
+          />
+        </div>
+      </SC.Content>
+    </main>
+  );
+};
 
 export default memo(NewsArticlePage);
