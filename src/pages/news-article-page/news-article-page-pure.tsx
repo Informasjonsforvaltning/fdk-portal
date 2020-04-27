67130bf3df58316ca1c71fc5d46d533c218adcc2
Øyvind Isaksen <oyvindisaks1@gmail.com>
2020-04-27 19:35:17 +0200

    feat(article): render all field_modules

--- src/pages/news-article-page/news-article-page-pure.tsx
+++ src/pages/news-article-page/news-article-page-pure.tsx
@@ -1,13 +1,7 @@
-import React, { FC, memo, useEffect, useState } from 'react';
+import React, { FC, memo } from 'react';
 
 import { News as NewsInterface } from '../../types';
-import {
-  getParagraphBodyValue,
-  getParagraphImage,
-  getParagraphVideoValue
-} from '../../lib/drupal/drupal-values';
 import Article from '../../components/article/components/article/article.component';
-import { getVimeoData } from '../../api/vimeo/host';
 
 interface Props {
   newsItem?: Partial<NewsInterface>;
@@ -17,27 +11,14 @@ interface Props {
 const NewsArticlePage: FC<Props> = ({
   newsItem: { created, title, field_ingress, field_modules } = {},
   news
-}) => {
-  const videoLinkValue = getParagraphVideoValue(field_modules);
-  const [vimeoData, setVimeoData] = useState();
-
-  useEffect(() => {
-    if (videoLinkValue) {
-      getVimeoData(videoLinkValue).then(setVimeoData);
-    }
-  }, []);
-
-  return (
-    <Article
-      publishedDate={created}
-      title={title}
-      abstract={field_ingress}
-      body={getParagraphBodyValue(field_modules)}
-      imageTop={getParagraphImage(field_modules)}
-      vimeoData={vimeoData}
-      relatedNews={news}
-    />
-  );
-};
+}) => (
+  <Article
+    publishedDate={created}
+    title={title}
+    abstract={field_ingress}
+    field_modules={field_modules}
+    relatedNews={news}
+  />
+);
 
 export default memo(NewsArticlePage);
