ab74ab5756a67d6c09e84a8445e960bc57b8e042
Øyvind Isaksen <oyvindisaks1@gmail.com>
2020-05-06 14:54:25 +0200

    chore(guidance-page): #317 add guidance page and resolve article from cms

---
+++ src/pages/guidance-page/guidance-page-pure.tsx
@@ -0,0 +1,22 @@
+import React, { FC, memo } from 'react';
+
+import { News as NewsInterface } from '../../types';
+import Article from '../../components/article/components/article/article.component';
+
+interface Props {
+  article?: Partial<NewsInterface>;
+}
+
+const GuidancePage: FC<Props> = ({
+  article: { created, changed, title, field_ingress, field_modules } = {}
+}) => (
+  <Article
+    publishedDate={created}
+    lastChangedDate={changed}
+    title={title}
+    abstract={field_ingress}
+    field_modules={field_modules}
+  />
+);
+
+export default memo(GuidancePage);
