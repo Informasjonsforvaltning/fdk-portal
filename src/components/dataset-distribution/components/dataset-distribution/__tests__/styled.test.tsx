3cdcfa781433b6408ade9dce0886713dc218cf88
Øyvind Isaksen <oyvindisaks1@gmail.com>
2020-05-07 10:19:01 +0200

    fix(dataset-distribution): #3076 modify color of HeadExpansionIndicator

--- src/components/dataset-distribution/components/dataset-distribution/__tests__/styled.test.tsx
+++ src/components/dataset-distribution/components/dataset-distribution/__tests__/styled.test.tsx
@@ -1,9 +1,12 @@
 import { cleanup } from '@testing-library/react';
 
+import { themeFDK as theme } from '../../../../../app/theme';
+
 import {
   Expectation,
   expectCorrectRootElement,
-  expectStyleRules
+  expectStyleRules,
+  renderWithTheme
 } from '../../../../../../test/utils';
 
 import { SC as ExpansionPanelSC } from '../../../../expansion-panel';
@@ -15,44 +18,51 @@ afterEach(cleanup);
 describe('Styled components for DatasetDistribution component', () => {
   describe('SC.DatasetDistribution component', () => {
     it(Expectation.ROOT_ELEMENT, () => {
-      expectCorrectRootElement(SC.DatasetDistribution, 'div');
+      expectCorrectRootElement(SC.DatasetDistribution, 'div', renderWithTheme, {
+        theme
+      });
     });
 
     it(Expectation.STYLE_RULES, () => {
-      expectStyleRules(SC.DatasetDistribution, [
-        { property: 'background', value: 'white' },
-        { property: 'border-radius', value: '5px' },
-        { property: 'overflow', value: 'hidden' },
-        {
-          property: 'margin-top',
-          value: '10px',
-          options: { modifier: '&:nth-of-type(n + 2)' }
-        },
-        {
-          property: 'padding',
-          value: '12px 24px',
-          options: { modifier: `& > ${ExpansionPanelSC.ExpansionPanel.Head}` }
-        },
-        {
-          property: 'min-width',
-          value: '0',
-          options: {
-            modifier: `& > ${ExpansionPanelSC.ExpansionPanel.Head} > ${ExpansionPanelSC.ExpansionPanel.HeadContent}`
+      expectStyleRules(
+        SC.DatasetDistribution,
+        [
+          { property: 'background', value: 'white' },
+          { property: 'border-radius', value: '5px' },
+          { property: 'overflow', value: 'hidden' },
+          {
+            property: 'margin-top',
+            value: '10px',
+            options: { modifier: '&:nth-of-type(n + 2)' }
+          },
+          {
+            property: 'padding',
+            value: '12px 24px',
+            options: { modifier: `& > ${ExpansionPanelSC.ExpansionPanel.Head}` }
+          },
+          {
+            property: 'min-width',
+            value: '0',
+            options: {
+              modifier: `& > ${ExpansionPanelSC.ExpansionPanel.Head} > ${ExpansionPanelSC.ExpansionPanel.HeadContent}`
+            }
+          },
+          {
+            property: 'margin-left',
+            value: '24px',
+            options: {
+              modifier: `& > ${ExpansionPanelSC.ExpansionPanel.Head} > ${ExpansionPanelSC.ExpansionPanel.HeadExpansionIndicator}`
+            }
+          },
+          {
+            property: 'padding',
+            value: '0px 24px 12px',
+            options: { modifier: `& > ${ExpansionPanelSC.ExpansionPanel.Body}` }
           }
-        },
-        {
-          property: 'margin-left',
-          value: '24px',
-          options: {
-            modifier: `& > ${ExpansionPanelSC.ExpansionPanel.Head} > ${ExpansionPanelSC.ExpansionPanel.HeadExpansionIndicator}`
-          }
-        },
-        {
-          property: 'padding',
-          value: '0px 24px 12px',
-          options: { modifier: `& > ${ExpansionPanelSC.ExpansionPanel.Body}` }
-        }
-      ]);
+        ],
+        renderWithTheme,
+        { theme }
+      );
     });
   });
 });
