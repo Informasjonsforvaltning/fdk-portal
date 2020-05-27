5149a262f414710ebaf94f115e62fa696428b0c5
Øyvind Isaksen <oyvindisaks1@gmail.com>
2020-05-27 09:06:58 +0200

    Revert "feat: #331 add organizations to navbar"
    This reverts commit d5cba116

--- src/app/app-nav-bar/app-nav-bar.jsx
+++ src/app/app-nav-bar/app-nav-bar.jsx
@@ -17,8 +17,7 @@ import {
   PATHNAME_ABOUT_REGISTRATION,
   PATHNAME_ABOUT_NAP,
   PATHNAME_HOME_NAP,
-  PATHNAME_GUIDANCE,
-  PATHNAME_ORGANIZATIONS
+  PATHNAME_GUIDANCE
 } from '../../constants/constants';
 import './app-nav-bar.scss';
 import { getConfig } from '../../config';
@@ -62,11 +61,6 @@ export function AppNavBar(props) {
                   <a className="nav-link" href={PATHNAME_GUIDANCE}>
                     {localization.menu.guidance}
                   </a>
-                </NavItem>
-                <NavItem>
-                  <Link className="nav-link" to={PATHNAME_ORGANIZATIONS}>
-                    {localization.menu.organizations}
-                  </Link>
                 </NavItem>
                 <NavItem>
                   <Link className="nav-link" to={PATHNAME_ABOUT}>
@@ -141,9 +135,6 @@ export function AppNavBar(props) {
                 <DropdownMenu right className="fdk-dropdownmenu">
                   <Link className="dropdown-item" to={PATHNAME_GUIDANCE}>
                     {localization.menu.guidance}
-                  </Link>
-                  <Link className="dropdown-item" to={PATHNAME_ORGANIZATIONS}>
-                    {localization.menu.organizations}
                   </Link>
                   <Link className="dropdown-item" to={PATHNAME_ABOUT}>
                     {localization.about.about}
