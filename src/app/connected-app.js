8f921d446c05936dc2abc4e78ff68f3059dd4f95
Øyvind Isaksen <oyvindisaks1@gmail.com>
2020-05-28 16:17:50 +0200

    refactor(app): onChangeLanguage as prop from app

--- src/app/connected-app.js
+++ src/app/connected-app.js
@@ -1,9 +1,14 @@
 import { connect } from 'react-redux';
 import { App } from './app';
+import { setLanguageAction } from '../redux/modules/settings';
 
 const mapStateToProps = state => ({
   // react-localization module designed to be stateful, so we have to re-render the app when the selected language is changed
   language: state.settings.language
 });
 
-export const ConnectedApp = connect(mapStateToProps)(App);
+const mapDispatchToProps = dispatch => ({
+  onChangeLanguage: language => dispatch(setLanguageAction(language))
+});
+
+export const ConnectedApp = connect(mapStateToProps, mapDispatchToProps)(App);
