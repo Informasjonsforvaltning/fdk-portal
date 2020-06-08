313a2a8eb1421a6e79e50fdbb46b7aa77e5fceee
Øyvind Isaksen <oyvindisaks1@gmail.com>
2020-06-08 15:46:29 +0200

    refactor(report-page): rename folder datasets-report-page to report-page

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -16,7 +16,7 @@ import { AboutPage } from '../pages/about-page/about-page.component';
 import { ArticlePage } from '../pages/article-page/article-page.component';
 import { Breadcrumbs } from './breadcrumbs/breadcrumbs.component';
 import { AppNavBar } from './app-nav-bar/app-nav-bar';
-import { ReportPage } from '../pages/datasets-report-page/report-page';
+import { ReportPage } from '../pages/report-page/report-page';
 import {
   PATHNAME_SEARCH,
   PATHNAME_DATASETS,
