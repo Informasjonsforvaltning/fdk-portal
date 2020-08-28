009fcefa8d2199ad92e60cf09b665eb82e76bc1c
Øyvind Isaksen <oyvindisaks1@gmail.com>
2020-08-28 12:44:55 +0200

    chore: linting

--- src/components/button-toggle/styled.ts
+++ src/components/button-toggle/styled.ts
@@ -5,45 +5,45 @@ const ButtonToggle = styled.button<{
   borderLeft?: boolean;
   borderRight?: boolean;
 }>`
-    background-color: ${({ theme }) => theme.extendedColors.neutralLight};
-    border: none;
-    padding: .5em;
-    ${({ borderLeft }) =>
-      borderLeft &&
-      css`
-        border-top-left-radius: 5px;
-        border-bottom-left-radius: 5px;
-      `}
-    
-    ${({ borderRight }) =>
-      borderRight &&
-      css`
-        border-top-right-radius: 5px;
-        border-bottom-right-radius: 5px;
-      `}    
+  background-color: ${({ theme }) => theme.extendedColors.neutralLight};
+  border: none;
+  padding: 0.5em;
+  ${({ borderLeft }) =>
+    borderLeft &&
+    css`
+      border-top-left-radius: 5px;
+      border-bottom-left-radius: 5px;
+    `}
+
+  ${({ borderRight }) =>
+    borderRight &&
+    css`
+      border-top-right-radius: 5px;
+      border-bottom-right-radius: 5px;
+    `}    
     
     ${({ selected }) =>
-      selected
-        ? css`
-            background-color: ${({ theme }) =>
-              theme.extendedColors.neutralDarker};
-            box-shadow: 0 4px 8px rgba(45, 55, 65, 0.2);
-            color: #fff;
-            &:before {
-              content: '\\F00C';
-              font: normal normal normal 12px/1 FontAwesome;
-              -webkit-font-smoothing: antialiased;
-              -moz-osx-font-smoothing: grayscale;
-              margin-right: 0.5em;
-            }
-          `
-        : css`
-            cursor: pointer;
-          `}
+    selected
+      ? css`
+          background-color: ${({ theme }) =>
+            theme.extendedColors.neutralDarker};
+          box-shadow: 0 4px 8px rgba(45, 55, 65, 0.2);
+          color: #fff;
+          &:before {
+            content: '\\F00C';
+            font: normal normal normal 12px/1 FontAwesome;
+            -webkit-font-smoothing: antialiased;
+            -moz-osx-font-smoothing: grayscale;
+            margin-right: 0.5em;
+          }
+        `
+      : css`
+          cursor: pointer;
+        `}
   
     @media (min-width: 992px) {
-      min-width: 14em;
-    }    
+    min-width: 14em;
+  }
 `;
 
 export default {
