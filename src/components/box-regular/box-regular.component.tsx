8a98b4cdc948dcb72ac5fdf69cd576738f4d6791
Øyvind Isaksen <oyvindisaks1@gmail.com>
2020-09-02 09:21:31 +0200

    feat(box-regular): add subHeader

--- src/components/box-regular/box-regular.component.tsx
+++ src/components/box-regular/box-regular.component.tsx
@@ -7,14 +7,17 @@ import { Variant } from './enums';
 interface Props {
   variant?: Variant;
   header?: string;
+  subHeader?: string;
 }
 const BoxRegular: FC<PropsWithChildren<Props>> = ({
   variant,
   header,
+  subHeader,
   children
 }) => (
   <SC.BoxRegular>
     {header && <SC.BoxHeader>{header}</SC.BoxHeader>}
+    {subHeader && <SC.BoxSubHeader>{subHeader}</SC.BoxSubHeader>}
     {children && <SC.BoxContent variant={variant}>{children}</SC.BoxContent>}
   </SC.BoxRegular>
 );
