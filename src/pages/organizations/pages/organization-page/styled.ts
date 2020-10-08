f56492ff1c07e4b7cb9c713f92acf973c2c8d1ce
Alexander Zakharov <alexander.zakharov@capgemini.com>
2020-10-08 12:19:22 +0200

    fix: add missing import

--- src/pages/organizations/pages/organization-page/styled.ts
+++ src/pages/organizations/pages/organization-page/styled.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { theme, Colour } from '@fellesdatakatalog/theme';
 
 import { getConfig } from '../../../../config';
