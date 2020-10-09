080c764b070c792dddec408b879150a93209d999
Øyvind Isaksen <oyvindisaks1@gmail.com>
2020-10-09 13:42:55 +0200

    feat: add cms articles for path about-registration

--- src/pages/cms-article-page/cms-article-page-resolver.ts
+++ src/pages/cms-article-page/cms-article-page-resolver.ts
@@ -4,13 +4,21 @@ import {
   getArticleEntity
 } from '../../api/cms-api/article-entities';
 import localization from '../../lib/localization';
-import { PATHNAME_GUIDANCE } from '../../constants/constants';
+import {
+  PATHNAME_ABOUT_REGISTRATION,
+  PATHNAME_GUIDANCE
+} from '../../constants/constants';
 
 const articleIds: { [pathname: string]: { [key: string]: string } } = {
   [PATHNAME_GUIDANCE]: {
     nb: 'e32a0d53-d5f7-4b07-adc2-cc8cbac0fcdb',
     nn: '40cb9799-4f53-482c-8075-417f70bcd483',
     en: '95430944-2177-48d1-9e1e-f589defd262c'
+  },
+  [PATHNAME_ABOUT_REGISTRATION]: {
+    nb: '1f8e5e49-c8a0-42bb-bab0-8948842d173f',
+    nn: 'd0f5c946-10d6-4416-9ec9-00ad95127a9f',
+    en: 'afb0ee91-dab4-44d7-857f-1652c3cac1b5'
   }
 };
 
