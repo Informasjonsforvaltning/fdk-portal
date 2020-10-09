a664329c15faf87d327dd9d2159a92f5a7156569
Øyvind Isaksen <oyvindisaks1@gmail.com>
2020-10-09 13:23:31 +0200

    refactor(guidance-page): articleIds linked to pathname

--- src/pages/guidance-page/guidance-page-resolver.ts
+++ src/pages/guidance-page/guidance-page-resolver.ts
@@ -4,16 +4,20 @@ import {
   getArticleEntity
 } from '../../api/cms-api/article-entities';
 import localization from '../../lib/localization';
+import { PATHNAME_GUIDANCE } from '../../constants/constants';
 
-const articleIds: { [key: string]: string } = {
-  nb: 'e32a0d53-d5f7-4b07-adc2-cc8cbac0fcdb',
-  nn: '40cb9799-4f53-482c-8075-417f70bcd483',
-  en: '95430944-2177-48d1-9e1e-f589defd262c'
+const articleIds: { [pathname: string]: { [key: string]: string } } = {
+  [PATHNAME_GUIDANCE]: {
+    nb: 'e32a0d53-d5f7-4b07-adc2-cc8cbac0fcdb',
+    nn: '40cb9799-4f53-482c-8075-417f70bcd483',
+    en: '95430944-2177-48d1-9e1e-f589defd262c'
+  }
 };
 
 const mapProps = {
-  article: () => {
-    const ARTICLE_FETCH_ID = articleIds[localization.getLanguage()];
+  article: ({ location }: any) => {
+    const ARTICLE_FETCH_ID =
+      articleIds[location.pathname]?.[localization.getLanguage()];
 
     return getArticleEntity(ARTICLE_FETCH_ID)
       .then(extractArticleData)
