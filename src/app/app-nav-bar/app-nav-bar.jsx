517f2c66140c7ba1955cfa149ff051fb755e2259
matsjsk <matsjorgen@hotmail.com>
2020-10-15 12:38:24 +0200

    fix: replace bootstrap menu dropdown

--- src/app/app-nav-bar/app-nav-bar.jsx
+++ src/app/app-nav-bar/app-nav-bar.jsx
@@ -10,6 +10,10 @@ import {
   NavItem
 } from 'reactstrap';
 
+import { ThemeProvider } from 'styled-components';
+
+import PortalDropdownMenu from '../../components/dropdown-menu/index';
+
 import localization from '../../lib/localization';
 import {
   PATHNAME_REPORTS,
@@ -20,192 +24,191 @@ import {
   PATHNAME_GUIDANCE,
   PATHNAME_ORGANIZATIONS
 } from '../../constants/constants';
+
+import { Entity } from '../../types/enums';
+
+import { themeFDK, themeNAP } from '../theme';
+
 import './app-nav-bar.scss';
 import { getConfig } from '../../config';
+
+const entity = Entity.DATASET;
+
+const theme = {
+  entityColours: (getConfig().themeNap ? themeNAP : themeFDK).extendedColors[
+    entity
+  ]
+};
+
+const getMenuItems = isNap => {
+  if (isNap) {
+    return [
+      {
+        label: localization.menu.aboutNap,
+        linkPath: PATHNAME_ABOUT_NAP
+      },
+      {
+        label: localization.menu.aboutRegistration,
+        linkPath: PATHNAME_ABOUT_REGISTRATION
+      }
+    ];
+  }
+  return [
+    {
+      label: localization.menu.guidance,
+      linkPath: PATHNAME_GUIDANCE
+    },
+    {
+      label: localization.menu.organizations,
+      linkPath: PATHNAME_ORGANIZATIONS
+    },
+    {
+      label: localization.about.about,
+      linkPath: PATHNAME_ABOUT
+    },
+    {
+      label: localization.menu.aboutRegistration,
+      linkPath: PATHNAME_ABOUT_REGISTRATION
+    },
+    {
+      label: localization.menu.reports,
+      linkPath: PATHNAME_REPORTS
+    }
+  ];
+};
 
 export function AppNavBar(props) {
   const fdkLogoPath = getConfig().useDemoLogo
     ? 'fdk-logo-demo.svg'
     : 'fdk-datanorge-logo.svg';
+  const languageItems = [
+    {
+      label: localization.lang['norwegian-nb'],
+      fn: () => props.onChangeLanguage('nb')
+    },
+    {
+      label: localization.lang['norwegian-nn'],
+      fn: () => props.onChangeLanguage('nn')
+    },
+    {
+      label: localization.lang['english-en'],
+      fn: () => props.onChangeLanguage('en')
+    }
+  ];
 
   return (
-    <div className="fdk-header">
-      <div className="container">
-        <div className="row d-flex justify-content-between align-items-center">
-          <div>
-            <a
-              title={
-                getConfig().themeNap
-                  ? localization.linkToNap
-                  : localization.linkToFdk
-              }
-              href={getConfig().themeNap ? PATHNAME_HOME_NAP : '/'}
-              className="d-flex"
-            >
-              <span className="uu-invisible" aria-hidden="false">
-                Gå til forside
-              </span>
-              <img
-                className={getConfig().themeNap ? 'nap-logo' : 'fdk-logo'}
-                src={
+    <ThemeProvider theme={theme}>
+      <div className="fdk-header">
+        <div className="container">
+          <div className="row d-flex justify-content-between align-items-center">
+            <div>
+              <a
+                title={
                   getConfig().themeNap
-                    ? '/img/logo-transport.svg'
-                    : `/img/${fdkLogoPath}`
+                    ? localization.linkToNap
+                    : localization.linkToFdk
                 }
-                alt="Logo for Felles datakatalog"
-              />
-            </a>
-          </div>
-          <div>
-            {!getConfig().themeNap && (
-              <Nav className="d-none d-lg-inline-flex">
-                <NavItem>
-                  <a className="nav-link" href={PATHNAME_GUIDANCE}>
-                    {localization.menu.guidance}
-                  </a>
-                </NavItem>
-                <NavItem>
-                  <Link className="nav-link" to={PATHNAME_ORGANIZATIONS}>
-                    {localization.menu.organizations}
-                  </Link>
-                </NavItem>
-                <NavItem>
-                  <Link className="nav-link" to={PATHNAME_ABOUT}>
-                    {localization.menu.about}
-                  </Link>
-                </NavItem>
-                <UncontrolledDropdown nav inNavbar>
-                  <DropdownToggle nav>{localization.help}</DropdownToggle>
-                  <DropdownMenu>
-                    <DropdownItem>
-                      <Link
-                        className="nav-link"
-                        to={PATHNAME_ABOUT_REGISTRATION}
-                      >
-                        {localization.menu.aboutRegistration}
-                      </Link>
-                    </DropdownItem>
-                  </DropdownMenu>
-                </UncontrolledDropdown>
-                <NavItem>
-                  <Link className="nav-link" to={PATHNAME_REPORTS}>
-                    {localization.menu.reports}
-                  </Link>
-                </NavItem>
-              </Nav>
-            )}
-            {getConfig().themeNap && (
-              <Nav className="d-none d-lg-inline-flex">
-                <NavItem>
-                  <a className="nav-link" href={PATHNAME_ABOUT_NAP}>
-                    {localization.menu.aboutNap}
-                  </a>
-                </NavItem>
-                <NavItem>
-                  <Link className="nav-link" to={PATHNAME_ABOUT_REGISTRATION}>
-                    {localization.menu.aboutRegistration}
-                  </Link>
-                </NavItem>
-                <NavItem>
-                  <Link className="nav-link" to={PATHNAME_ORGANIZATIONS}>
-                    {localization.menu.organizations}
-                  </Link>
-                </NavItem>
-                <NavItem>
-                  <Link className="nav-link" to={PATHNAME_REPORTS}>
-                    {localization.menu.reports}
-                  </Link>
-                </NavItem>
-              </Nav>
-            )}
-          </div>
-          <div>
-            <UncontrolledDropdown className="d-none d-lg-inline">
-              <DropdownToggle className="fdk-button-lang" caret>
-                {localization.lang.chosenLanguage}
-              </DropdownToggle>
-              <DropdownMenu right className="fdk-dropdownmenu">
-                <DropdownItem onClick={() => props.onChangeLanguage('nb')}>
-                  {localization.lang['norwegian-nb']}
-                </DropdownItem>
-                <DropdownItem onClick={() => props.onChangeLanguage('nn')}>
-                  {localization.lang['norwegian-nn']}
-                </DropdownItem>
-                <DropdownItem onClick={() => props.onChangeLanguage('en')}>
-                  {localization.lang['english-en']}
-                </DropdownItem>
-              </DropdownMenu>
-            </UncontrolledDropdown>
-
-            <UncontrolledDropdown
-              tabIndex="0"
-              className="fdk-dropdown-menu d-inline d-lg-none"
-            >
-              <DropdownToggle
-                className="fdk-button fdk-button-menu"
-                caret
-                color="none"
+                href={getConfig().themeNap ? PATHNAME_HOME_NAP : '/'}
+                className="d-flex"
               >
-                {localization.app.menu}
-              </DropdownToggle>
+                <span className="uu-invisible" aria-hidden="false">
+                  Gå til forside
+                </span>
+                <img
+                  className={getConfig().themeNap ? 'nap-logo' : 'fdk-logo'}
+                  src={
+                    getConfig().themeNap
+                      ? '/img/logo-transport.svg'
+                      : `/img/${fdkLogoPath}`
+                  }
+                  alt="Logo for Felles datakatalog"
+                />
+              </a>
+            </div>
+            <div>
               {!getConfig().themeNap && (
-                <DropdownMenu right className="fdk-dropdownmenu">
-                  <Link className="dropdown-item" to={PATHNAME_GUIDANCE}>
-                    {localization.menu.guidance}
-                  </Link>
-                  <Link className="dropdown-item" to={PATHNAME_ORGANIZATIONS}>
-                    {localization.menu.organizations}
-                  </Link>
-                  <Link className="dropdown-item" to={PATHNAME_ABOUT}>
-                    {localization.about.about}
-                  </Link>
-                  <Link
-                    className="dropdown-item"
-                    to={PATHNAME_ABOUT_REGISTRATION}
-                  >
-                    {localization.menu.aboutRegistration}
-                  </Link>
-                  <Link className="dropdown-item" to={PATHNAME_REPORTS}>
-                    {localization.menu.reports}
-                  </Link>
-                  <DropdownItem onClick={() => props.onChangeLanguage('nb')}>
-                    {localization.lang['norwegian-nb']}
-                  </DropdownItem>
-                  <DropdownItem onClick={() => props.onChangeLanguage('nn')}>
-                    {localization.lang['norwegian-nn']}
-                  </DropdownItem>
-                  <DropdownItem onClick={() => props.onChangeLanguage('en')}>
-                    {localization.lang['english-en']}
-                  </DropdownItem>
-                </DropdownMenu>
+                <Nav className="d-none d-lg-inline-flex">
+                  <NavItem>
+                    <a className="nav-link" href={PATHNAME_GUIDANCE}>
+                      {localization.menu.guidance}
+                    </a>
+                  </NavItem>
+                  <NavItem>
+                    <Link className="nav-link" to={PATHNAME_ORGANIZATIONS}>
+                      {localization.menu.organizations}
+                    </Link>
+                  </NavItem>
+                  <NavItem>
+                    <Link className="nav-link" to={PATHNAME_ABOUT}>
+                      {localization.menu.about}
+                    </Link>
+                  </NavItem>
+                  <UncontrolledDropdown nav inNavbar>
+                    <DropdownToggle nav>{localization.help}</DropdownToggle>
+                    <DropdownMenu>
+                      <DropdownItem>
+                        <Link
+                          className="nav-link"
+                          to={PATHNAME_ABOUT_REGISTRATION}
+                        >
+                          {localization.menu.aboutRegistration}
+                        </Link>
+                      </DropdownItem>
+                    </DropdownMenu>
+                  </UncontrolledDropdown>
+                  <NavItem>
+                    <Link className="nav-link" to={PATHNAME_REPORTS}>
+                      {localization.menu.reports}
+                    </Link>
+                  </NavItem>
+                </Nav>
               )}
               {getConfig().themeNap && (
-                <DropdownMenu right className="fdk-dropdownmenu">
-                  <Link className="dropdown-item" to={PATHNAME_ABOUT_NAP}>
-                    {localization.menu.aboutNap}
-                  </Link>
-                  <Link
-                    className="dropdown-item"
-                    to={PATHNAME_ABOUT_REGISTRATION}
-                  >
-                    {localization.menu.aboutRegistration}
-                  </Link>
-                  <DropdownItem onClick={() => props.onChangeLanguage('nb')}>
-                    {localization.lang['norwegian-nb']}
-                  </DropdownItem>
-                  <DropdownItem onClick={() => props.onChangeLanguage('nn')}>
-                    {localization.lang['norwegian-nn']}
-                  </DropdownItem>
-                  <DropdownItem onClick={() => props.onChangeLanguage('en')}>
-                    {localization.lang['english-en']}
-                  </DropdownItem>
-                </DropdownMenu>
+                <Nav className="d-none d-lg-inline-flex">
+                  <NavItem>
+                    <a className="nav-link" href={PATHNAME_ABOUT_NAP}>
+                      {localization.menu.aboutNap}
+                    </a>
+                  </NavItem>
+                  <NavItem>
+                    <Link className="nav-link" to={PATHNAME_ABOUT_REGISTRATION}>
+                      {localization.menu.aboutRegistration}
+                    </Link>
+                  </NavItem>
+                  <NavItem>
+                    <Link className="nav-link" to={PATHNAME_ORGANIZATIONS}>
+                      {localization.menu.organizations}
+                    </Link>
+                  </NavItem>
+                  <NavItem>
+                    <Link className="nav-link" to={PATHNAME_REPORTS}>
+                      {localization.menu.reports}
+                    </Link>
+                  </NavItem>
+                </Nav>
               )}
-            </UncontrolledDropdown>
+            </div>
+            <div>
+              <PortalDropdownMenu
+                desktopView
+                caret
+                title={localization.lang.chosenLanguage}
+                menuItems={[...languageItems]}
+              />
+              <PortalDropdownMenu
+                mobileView
+                caret
+                title={localization.app.menu}
+                menuItems={[
+                  ...getMenuItems(getConfig().themeNap),
+                  ...languageItems
+                ]}
+              />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </ThemeProvider>
   );
 }
 
