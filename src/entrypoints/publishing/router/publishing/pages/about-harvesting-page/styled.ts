8f444967e110742134652e6d557cefd5ffe02ac6
Alexander Zakharov <alexander.zakharov@capgemini.com>
2020-10-23 15:25:06 +0200

    style: allow content to determine page height

--- src/entrypoints/publishing/router/publishing/pages/about-harvesting-page/styled.ts
+++ src/entrypoints/publishing/router/publishing/pages/about-harvesting-page/styled.ts
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import { theme } from '@fellesdatakatalog/theme';
 
 const AboutHarvestingPage = styled.article`
+  flex: 1;
   margin-top: ${theme.spacing('S48')};
 `;
 
--- src/entrypoints/publishing/router/publishing/pages/about-registration-page/styled.ts
+++ src/entrypoints/publishing/router/publishing/pages/about-registration-page/styled.ts
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import { theme } from '@fellesdatakatalog/theme';
 
 const AboutRegistrationPage = styled.article`
+  flex: 1;
   margin-top: ${theme.spacing('S48')};
 `;
 
