1c68c829ca50933b72eedd4475cb4f6e31fb06f3
Alexander Zakharov <alexander.zakharov@capgemini.com>
2020-11-09 10:47:36 +0100

    style: implement responsive design for publishing page

--- src/entrypoints/publishing/components/root/styled.ts
+++ src/entrypoints/publishing/components/root/styled.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { theme } from '@fellesdatakatalog/theme';
 
 const Root = styled.main`
   display: flex;
@@ -7,6 +8,19 @@ const Root = styled.main`
   height: 80px;
   width: 1140px;
   margin: 0 auto;
+
+  @media (max-width: 1204px) {
+    & {
+      width: auto;
+      margin: 0 ${theme.spacing('S32')};
+    }
+  }
+
+  @media (max-width: 900px) {
+    & {
+      margin: 0 calc(12px + (32 - 12) * ((100vw - 320px) / (900 - 320)));
+    }
+  }
 `;
 
 export default { Root };
