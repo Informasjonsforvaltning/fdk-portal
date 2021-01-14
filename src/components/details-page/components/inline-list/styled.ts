2a9b214b72124068740192ff30cece3038755719
matsjsk <matsjorgen@hotmail.com>
2021-01-14 15:22:05 +0100

    feat: adds newline after dataset and dataservice relations

--- src/components/details-page/components/inline-list/styled.ts
+++ src/components/details-page/components/inline-list/styled.ts
@@ -1,11 +1,21 @@
 import styled from 'styled-components';
 
-const List = styled.ul`
+const List = styled.ul<{ column: boolean }>`
   display: flex;
   flex-wrap: wrap;
   margin: -10px -10px 0 0;
   padding: 0;
   list-style: none;
+  flex-direction: ${({ column }) => (column ? 'column' : 'row')};
+  & > li {
+    padding-top: ${({ column }) => (column ? '5px' : '0px')};
+    border-top: ${({ column, theme }) =>
+      column ? `1px solid ${theme.entityColours.light}` : 'none'};
+  }
+  & > :first-child {
+    border-top: 0px;
+    padding-top: 0px;
+  }
 `;
 
 const ListItem = styled.li`
