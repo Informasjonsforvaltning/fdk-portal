c5e1a4f6a9ffd4c99faeacbdeec4b9ab2fe190d7
Øyvind Isaksen <oyvindisaks1@gmail.com>
2021-01-21 20:46:45 +0100

    fix(content-section): extend component style and add icons

--- src/components/details-page/components/content-section/styled.ts
+++ src/components/details-page/components/content-section/styled.ts
@@ -1,17 +1,99 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { theme, Colour } from '@fellesdatakatalog/theme';
 
-const ContentSection = styled.section`
+import DatasetIconBase from '../../../../images/icon-catalog-dataset-lg.svg';
+import ApiIconBase from '../../../../images/icon-catalog-api-lg.svg';
+import ConceptIconBase from '../../../../images/icon-catalog-concept-lg.svg';
+import InfomodIconBase from '../../../../images/icon-catalog-infomod-lg.svg';
+import ServiceIconBase from '../../../../images/icon-catalog-service-lg.svg';
+
+interface Props {
+  boxStyle?: boolean;
+}
+
+const ContentSection = styled.section<Props>`
   &:nth-of-type(n + 2) {
     margin-top: 50px;
   }
+
+  ${({ boxStyle }) =>
+    boxStyle &&
+    css`
+      background-color: ${theme.colour(Colour.NEUTRAL, 'N0')};
+      border-radius: 5px;
+      padding: ${theme.spacing('S32')};
+    `}
 `;
 
 const Title = styled.h2`
-  margin-bottom: 10px;
-  padding: 10px 0;
+  flex-grow: 1;
   font-size: 32px;
   font-weight: 600;
-  border-bottom: 1px solid ${({ theme }) => theme.entityColours.light};
 `;
 
-export default { ContentSection, Title };
+const Header = styled.div`
+  align-items: center;
+  border-bottom: 1px solid ${({ theme }) => theme.entityColours.light};
+  display: flex;
+  margin-bottom: ${theme.spacing('S10')};;
+  padding: ${theme.spacing('S10')}; 0;
+`;
+
+const IconPlaceholder = styled.div`
+  border-radius: 50%;
+  background-color: ${({ theme }) => theme.entityColours.light};
+  margin-right: ${theme.spacing('S16')};
+  padding: ${theme.spacing('S6')}; ;
+`;
+
+const DatasetIcon = styled(DatasetIconBase)`
+  height: 2em;
+
+  & > path {
+    fill: ${({ theme }) => theme.entityColours.dark};
+  }
+`;
+
+const ApiIcon = styled(ApiIconBase)`
+  height: 2em;
+
+  & > path {
+    fill: ${({ theme }) => theme.entityColours.dark};
+  }
+`;
+
+const ConceptIcon = styled(ConceptIconBase)`
+  height: 2em;
+
+  & > path {
+    fill: ${({ theme }) => theme.entityColours.dark};
+  }
+`;
+
+const InfomodIcon = styled(InfomodIconBase)`
+  height: 2em;
+
+  & > path {
+    fill: ${({ theme }) => theme.entityColours.dark};
+  }
+`;
+
+const ServiceIcon = styled(ServiceIconBase)`
+  height: 2em;
+
+  & > path {
+    fill: ${({ theme }) => theme.entityColours.dark};
+  }
+`;
+
+export default {
+  ContentSection,
+  Title,
+  Header,
+  IconPlaceholder,
+  DatasetIcon,
+  ApiIcon,
+  ConceptIcon,
+  InfomodIcon,
+  ServiceIcon
+};
