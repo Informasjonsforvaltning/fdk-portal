936185e089c0a617e2938a24ac239df2ef44842a
Jeff Reiffers <jeffreiffers@gmail.com>
2021-03-03 15:05:50 +0100

    fix: less strict format icon rendering

--- src/components/dataset-distribution/components/summary/index.tsx
+++ src/components/dataset-distribution/components/summary/index.tsx
@@ -13,12 +13,21 @@ interface Props {
   formats: string[];
 }
 
+const toDataFormat = (format: string) =>
+  Object.values(DataFormat).find(
+    v =>
+      format.toLowerCase().includes(v) ||
+      format.toLowerCase().includes(v.replace('text/', '')) ||
+      format.toLowerCase().includes(v.replace('application/', '')) ||
+      format.toLowerCase().includes(v.replace('application/vnd.', ''))
+  ) ?? DataFormat.UNKNOWN;
+
 const Summary: FC<Props> = ({ title, formats, ...props }) => (
   <SC.Summary data-testid={testIds.root} {...props}>
     <SC.Title data-testid={testIds.title}>{title}</SC.Title>
     <SC.Formats data-testid={testIds.formats}>
       {formats.map(format => {
-        const Icon = formatIcons[format] ?? formatIcons[DataFormat.UNKNOWN];
+        const Icon = formatIcons[toDataFormat(format)];
         return (
           <SC.Format key={format} data-testid={testIds.format}>
             <Icon />
