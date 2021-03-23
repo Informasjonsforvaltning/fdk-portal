b68abd6c6fd1428c8f307b22df40a477a11f3630
matsjs <matsjorgen@hotmail.com>
2021-03-23 16:24:49 +0100

    fix: adds header and footer to root error page

--- src/components/error-page/index.tsx
+++ src/components/error-page/index.tsx
@@ -1,12 +1,20 @@
 import React, { FC } from 'react';
+import cx from 'classnames';
+
+import { getConfig } from '../../config';
+import localization from '../../lib/localization';
 
 import Error400 from './components/error-400';
 import Error500 from './components/error-500';
 
 import SC from './styled';
+import Header from '../header';
+import Footer from '../footer';
 
 interface Props {
   errorCode?: string;
+  language?: string;
+  onChangeLanguage?: (language: string) => void;
 }
 
 const errorMessage = (errorCode?: string) => {
@@ -19,12 +27,33 @@ const errorMessage = (errorCode?: string) => {
   }
 };
 
-const ErrorPage: FC<Props> = ({ errorCode }) => (
-  <SC.ErrorPage>
-    {errorMessage(errorCode)}
-    <SC.UpperRightBackground />
-    <SC.LowerLeftBackground />
-  </SC.ErrorPage>
-);
+const themeClass = cx('position-relative overflow-hidden', {
+  'theme-nap': getConfig().themeNap,
+  'theme-fdk': !getConfig().themeNap
+});
+
+const ErrorPage: FC<Props> = ({ errorCode, language, onChangeLanguage }) => {
+  if (language && onChangeLanguage) {
+    localization.setLanguage(language);
+  }
+
+  return language && onChangeLanguage ? (
+    <div className={themeClass}>
+      <Header onChangeLanguage={onChangeLanguage} />
+      <SC.ErrorPage>
+        {errorMessage(errorCode)}
+        <SC.UpperRightBackground />
+        <SC.LowerLeftBackground />
+      </SC.ErrorPage>
+      <Footer />
+    </div>
+  ) : (
+    <SC.ErrorPage>
+      {errorMessage(errorCode)}
+      <SC.UpperRightBackground />
+      <SC.LowerLeftBackground />
+    </SC.ErrorPage>
+  );
+};
 
 export default ErrorPage;
