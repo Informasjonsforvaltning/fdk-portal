c97b7a5ab5c8c4da3f175c021a26bf524c3c25a2
Øyvind Isaksen <oyvindisaks1@gmail.com>
2021-04-22 10:26:59 +0200

    feat(concept-details-page): sort fields based on language

---
+++ src/lib/languageSorter.ts
@@ -0,0 +1,27 @@
+import { Language } from '../providers/translations';
+import type { TextLanguage } from '../types';
+
+const priorities: Record<string, number> = {
+  [Language.NB]: 1,
+  [Language.NN]: 2,
+  [Language.EN]: 3
+};
+
+export const languageSorter = (languageArray: Partial<TextLanguage>[]) => {
+  return languageArray.sort(
+    (a: Partial<TextLanguage>, b: Partial<TextLanguage>) => {
+      const aKey = Object.keys(a)[0];
+      const bKey = Object.keys(b)[0];
+
+      if (!(aKey in priorities)) {
+        return 1;
+      }
+
+      if (!(bKey in priorities)) {
+        return -1;
+      }
+
+      return priorities[aKey] > priorities[bKey] ? 1 : -1;
+    }
+  );
+};
