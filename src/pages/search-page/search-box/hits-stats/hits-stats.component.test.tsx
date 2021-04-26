525f9bb0ecb6bbacbc37180ce9a4005e1c8c29dd
Øyvind Isaksen <oyvindisaks1@gmail.com>
2021-04-26 11:15:11 +0200

    refactor(hits-stats): use typing and simplify

--- src/pages/search-page/search-box/hits-stats/hits-stats.component.test.tsx
+++ src/pages/search-page/search-box/hits-stats/hits-stats.component.test.tsx
@@ -5,17 +5,16 @@ import { HitsStats } from './hits-stats.component';
 test('should render HitsStats correctly with hits ', () => {
   const defaultProps = {
     countDatasets: 100,
-    countTerms: 100
+    countApis: 100,
+    countTerms: 100,
+    countInformationModels: 100
   };
   const wrapper = shallow(<HitsStats {...defaultProps} />);
   expect(wrapper).toMatchSnapshot();
 });
 
 test('should render HitsStats correctly when no hits', () => {
-  const defaultProps = {
-    countDatasets: 0,
-    countTerms: 0
-  };
+  const defaultProps = {};
   const wrapper = shallow(<HitsStats {...defaultProps} />);
   expect(wrapper).toMatchSnapshot();
 });
