935374ec58cfb958f7c6c25a0d980cc2528b13c1
Øyvind Isaksen <oyvindisaks1@gmail.com>
2021-04-26 09:32:34 +0200

    fix(search-box): add style as named export

--- src/components/search-box/search-box.ts
+++ src/components/search-box/search-box.ts
@@ -1,5 +1,9 @@
+import SearchBoxSC from './components/search-box/styled';
+
 export { default as SearchBox } from './components/search-box/search-box.component';
 
 export { default as SearchLink } from './components/search-link/search-link.component';
 
 export { default as SearchBoxHeader } from './components/search-box-header/search-box-header.component';
+
+export const SC = { SearchBox: SearchBoxSC };
