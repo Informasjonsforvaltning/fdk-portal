99946b72f5c1857535924168287b0c5c6ac9ccf1
matsjs <matsjorgen@hotmail.com>
2021-07-06 17:00:56 +0200

    chore: update news list styling

--- src/components/news-list/styled.ts
+++ src/components/news-list/styled.ts
@@ -1,14 +1,17 @@
 import styled from 'styled-components';
-import { Link as LinkBase } from 'react-router-dom';
+import LinkBase from '@fellesdatakatalog/link';
+import { theme as themeFDK } from '@fellesdatakatalog/theme';
 
 const NewsList = styled.div`
   display: flex;
   flex-flow: column;
+  color: ${({ theme }) => theme.extendedColors.neutralDarker} !important;
 `;
 
 const Link = styled(LinkBase)`
-  color: ${({ theme }) => theme.extendedColors.neutralDarker} !important;
-  text-decoration: underline;
+  font-size: ${themeFDK.fontSize('FS16')};
+  font-weight: ${themeFDK.fontWeight('FW700')};
+  margin-top: ${themeFDK.spacing('S8')};
 `;
 
 export default {
