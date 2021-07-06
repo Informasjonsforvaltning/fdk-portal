eed6d33785651e8e4adbe99c7c367183eb4763df
matsjs <matsjorgen@hotmail.com>
2021-07-06 17:02:59 +0200

    feat: adds datalandsbyen home link

--- src/pages/main-page/components/header/styled.ts
+++ src/pages/main-page/components/header/styled.ts
@@ -3,11 +3,23 @@ import styled from 'styled-components';
 const Header = styled.h2`
   font-size: 3rem;
   font-weight: 600;
-  margin-bottom: 1em;
+  margin-top: 1em;
+  margin-bottom: 0.25em;
   text-align: center;
 
   @media (min-width: 992px) {
     text-align: left;
+  }
+
+  & > a {
+    font-size: inherit;
+    font-weight: inherit;
+    margin: none;
+
+    & svg {
+      height: 3rem;
+      width: 3rem;
+    }
   }
 `;
 
