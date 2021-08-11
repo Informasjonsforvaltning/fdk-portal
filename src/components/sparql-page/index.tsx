4d44487c336bdf75ca56fd1a79dbc045984f80b9
matsjs <matsjorgen@hotmail.com>
2021-08-11 15:06:54 +0200

    feat: set sparql-endpoint through prop

--- src/components/sparql-page/index.tsx
+++ src/components/sparql-page/index.tsx
@@ -1,10 +1,12 @@
 import React, { memo, FC, Suspense } from 'react';
 import { compose } from 'redux';
 
+import env from '../../env';
 import SC from './styled';
 
 // eslint-disable-next-line import/no-unresolved
 const SparqlGui = React.lazy(() => import('sparql_gui/SparqlGui'));
+const { FDK_SPARQL_API_BASE_URI } = env;
 
 interface Props {
   language: any;
@@ -13,7 +15,7 @@ interface Props {
 const SparqlPage: FC<Props> = ({ language }) => (
   <SC.SparqlPage className='container'>
     <Suspense fallback={null}>
-      <SparqlGui language={language} />
+      <SparqlGui language={language} endpoint={FDK_SPARQL_API_BASE_URI} />
     </Suspense>
   </SC.SparqlPage>
 );
