a97264bfbb48aed6512062824708fdfdacdcfb12
Øyvind Isaksen <oyvindisaks1@gmail.com>
2021-08-19 12:18:10 +0200

    feat(service-messages): add service message list page and details page

--- src/entrypoints/publishing/bootstrap.tsx
+++ src/entrypoints/publishing/bootstrap.tsx
@@ -4,17 +4,26 @@ import 'regenerator-runtime/runtime';
 import React from 'react';
 import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
 import App from './app';
+import { getConfig } from '../../config';
 
 import LoggingProvider from '../../providers/logging';
+
+const client = new ApolloClient({
+  uri: `${getConfig().cmsV2Api.host}/graphql`,
+  cache: new InMemoryCache()
+});
 
 function run(): void {
   render(
     <LoggingProvider>
-      <AppContainer>
-        <App />
-      </AppContainer>
+      <ApolloProvider client={client}>
+        <AppContainer>
+          <App />
+        </AppContainer>
+      </ApolloProvider>
     </LoggingProvider>,
     document.getElementById('root')
   );
---
+++ src/entrypoints/publishing/router/publishing/pages/service-message-page/index.tsx
@@ -0,0 +1,29 @@
+import React, { FC, memo } from 'react';
+import { useParams } from 'react-router-dom';
+
+import {
+  ServiceMessage,
+  useGetServiceMessageQuery
+} from '../../../../../../api/generated/cms/graphql';
+
+import SC from './styled';
+import Markdown from '../../../../../../components/markdown';
+
+interface Props {}
+
+const ServiceMessagePage: FC<Props> = () => {
+  const { id } = useParams<{ id: string }>();
+  const { data } = useGetServiceMessageQuery({ variables: { id } });
+  const serviceMessage = data?.serviceMessage as ServiceMessage;
+  return serviceMessage ? (
+    <SC.ServiceMessagePage>
+      <SC.Title>{serviceMessage.title}</SC.Title>
+      <SC.Description>{serviceMessage.short_description}</SC.Description>
+      <SC.Body>
+        <Markdown>{serviceMessage.description}</Markdown>
+      </SC.Body>
+    </SC.ServiceMessagePage>
+  ) : null;
+};
+
+export default memo(ServiceMessagePage);
