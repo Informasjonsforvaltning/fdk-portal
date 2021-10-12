e29b13cf0fd8d8b4d75afee1f898ebabc8594917
Frederik Rønnevig <frederik.code@gmail.com>
2021-10-12 08:47:23 +0200

    chore: apply project standards for colour values

--- src/entrypoints/publishing/components/header/styled.ts
+++ src/entrypoints/publishing/components/header/styled.ts
@@ -59,96 +59,11 @@ const DemoLogo = styled(DemoLogoSVG)`
   height: 55px;
 
   .st0 {
-    fill: #ffffff;
+    fill: ${theme.colour(Colour.NEUTRAL, 'N0')};
   }
 
   .st1 {
-    fill: #afc2db;
-  }
-
-  .st2 {
-    display: none;
-  }
-
-  .st3 {
-    display: inline;
-    fill: none;
-    stroke: #c2132c;
-    stroke-width: 4.8e-2;
-    stroke-miterlimit: 10;
-  }
-
-  .st4 {
-    display: inline;
-    fill: none;
-    stroke: #c2132c;
-    stroke-width: 5e-2;
-    stroke-miterlimit: 10;
-  }
-
-  .st5 {
-    display: inline;
-    fill: none;
-    stroke: #1eacf5;
-    stroke-width: 5e-2;
-    stroke-miterlimit: 10;
-  }
-
-  .st6 {
-    display: inline;
-    fill: none;
-    stroke: #c2132c;
-    stroke-width: 5.2e-2;
-    stroke-miterlimit: 10;
-  }
-
-  .st7 {
-    display: inline;
-    fill: #c2132c;
-  }
-
-  .st8 {
-    font-family: 'NBInternationalPro-Reg';
-  }
-
-  .st9 {
-    font-size: 4px;
-  }
-
-  .st10 {
-    font-size: 3px;
-  }
-
-  .st11 {
-    display: inline;
-  }
-
-  .st12 {
-    fill: none;
-    stroke: #c2132c;
-    stroke-width: 4.8e-2;
-    stroke-miterlimit: 10;
-  }
-
-  .st13 {
-    fill: none;
-    stroke: #c2132c;
-    stroke-width: 5e-2;
-    stroke-miterlimit: 10;
-  }
-
-  .st14 {
-    fill: none;
-    stroke: #c2132c;
-    stroke-width: 5.2e-2;
-    stroke-miterlimit: 10;
-  }
-
-  .st15 {
-    fill: none;
-    stroke: #1eacf5;
-    stroke-width: 5e-2;
-    stroke-miterlimit: 10;
+    fill: ${theme.colour(Colour.CYAN, 'C30')};
   }
 
   & > path {
