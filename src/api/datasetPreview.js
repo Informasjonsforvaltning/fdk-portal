1403cfc90f27978abfed4d61a8d7f34f2a50a55c
Jeff Reiffers <jeffreiffers@gmail.com>
2021-12-01 13:44:11 +0100

    chore: preview service api key auth

--- src/api/datasetPreview.js
+++ src/api/datasetPreview.js
@@ -1,11 +1,19 @@
 import axios from 'axios';
 import { getConfig } from '../config';
+import env from '../env';
+
+const { FDK_DATASET_PREVIEW_API_KEY } = env;
 
 export const getDatasetPreview = (url, rows) =>
   axios
     .post(
       `${getConfig().searchHost.host}/dataset/preview`,
       { url, rows },
-      getConfig().searchHost.config
+      {
+        ...getConfig().searchHost.config,
+        headers: {
+          'X-API-KEY': FDK_DATASET_PREVIEW_API_KEY
+        }
+      }
     )
     .then(r => r.data);
