3f2a7c56a2d5fd79d78560b3c5a0d45a796b4ad8
Jeff Reiffers <jeffreiffers@gmail.com>
2022-01-20 13:18:11 +0100

    chore: remove optimization

--- webpack/prod.config.ts
+++ webpack/prod.config.ts
@@ -13,37 +13,6 @@ const configuration: Configuration = merge(baseConfig, {
   output: {
     filename: '[name].[contenthash].js'
   },
-  optimization: {
-    splitChunks: {
-      maxSize: 40000,
-      cacheGroups: {
-        mainVendors: {
-          test: ({ resource = '' }: any) => resource.includes('node_modules'),
-          name: module =>
-            `main.vendor.${module.context
-              .match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1]
-              .replace('@', '')}`,
-          chunks: ({ name }) => name === 'main'
-        },
-        publishingVendors: {
-          test: ({ resource = '' }: any) => resource.includes('node_modules'),
-          name: module =>
-            `publishing.vendor.${module.context
-              .match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1]
-              .replace('@', '')}`,
-          chunks: ({ name }) => name === 'publishing'
-        },
-        maintenanceVendors: {
-          test: ({ resource = '' }: any) => resource.includes('node_modules'),
-          name: module =>
-            `maintenance.vendor.${module.context
-              .match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1]
-              .replace('@', '')}`,
-          chunks: ({ name }) => name === 'maintenance'
-        }
-      }
-    }
-  },
   plugins: [
     new ModuleFederationPlugin({
       name: 'portal',
