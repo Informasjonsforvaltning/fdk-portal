d8194363bc71cb526ee3434c6feecffd178df09c
pooriamehregan <pooriamehregan@gmail.com>
2022-04-20 14:05:07 +0200

    fix: change offentlig transport to kollektivtransport

--- src/pages/main-page/main-page-pure.tsx
+++ src/pages/main-page/main-page-pure.tsx
@@ -41,7 +41,7 @@ const MainPage: FC<Props> = ({
 
   return (
     <div>
-      <SearchBox placeholder='Eksempel: offentlig transport' autosuggest>
+      <SearchBox placeholder='Eksempel: kollektivtransport' autosuggest>
         <SearchBoxHeader as='h1' large>
           {localization.searchBoxHeader}
         </SearchBoxHeader>
