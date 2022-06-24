b1a4e03d481d2a284bbb47ffbfc0a62995a8aeb0
pooriamehregan <pooriamehregan@gmail.com>
2022-06-24 11:53:10 +0200

    style: integrate new elements in communit post

--- src/components/community/post/index.tsx
+++ src/components/community/post/index.tsx
@@ -1,17 +1,11 @@
 import React, { FC } from 'react';
 import parse from 'html-react-parser';
-
 import env from '../../../env';
-
-import translations from '../../../lib/localization';
-
-import TimeAgo from '../time-ago';
 import User from '../user';
-import TruncatedText from '../../truncated-text';
-
 import SC from './styled';
-
 import type { CommunityPost } from '../../../types';
+import TimeStamp from '../time-stamp';
+import TruncatedText from '../../truncated-text';
 
 const { FDK_COMMUNITY_BASE_URI } = env;
 
@@ -23,19 +17,14 @@ const PostLink: FC<Props> = ({ post }) => (
   <SC.PostLink
     href={`${FDK_COMMUNITY_BASE_URI}/topic/${post.topic.slug}/${post.pid}`}
   >
-    <h3>{post.topic.titleRaw}</h3>
-    <SC.Content>
-      <SC.UserInfo>
-        <User user={post.user} />
-        <span>
-          {translations.community.timeago.postPrefix}
-          <TimeAgo startTime={post.timestamp} lowercase />:
-        </span>
-      </SC.UserInfo>
-      <TruncatedText visibleLines={5} lineHeight={14} onlyTruncate>
-        {parse(post.content)}
-      </TruncatedText>
-    </SC.Content>
+    <h5>{post.topic.titleRaw}</h5>
+    <SC.UserInfo>
+      <User user={post.user} />
+    </SC.UserInfo>
+    <TruncatedText visibleLines={4} lineHeight={24} onlyTruncate>
+      {parse(post.content)}
+    </TruncatedText>
+    <TimeStamp time={post.timestamp} />
   </SC.PostLink>
 );
 
