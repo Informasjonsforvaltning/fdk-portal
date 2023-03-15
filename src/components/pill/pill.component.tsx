873b479578716788acd9fea7f370269d299b85c2
Jeff Reiffers <jeffreiffers@gmail.com>
2023-03-15 14:22:15 +0100

    fix: add arialabel for clear button

--- src/components/pill/pill.component.tsx
+++ src/components/pill/pill.component.tsx
@@ -1,12 +1,14 @@
 import React, { FC, HTMLAttributes, memo } from 'react';
+import localization from '../../lib/localization';
 
 import SC from './styled';
 
 interface Props extends HTMLAttributes<HTMLElement> {
   label: string;
+  onClear: () => void;
 }
 
-const PillPure: FC<Props> = ({ label, ...props }) => {
+const PillPure: FC<Props> = ({ label, onClear }) => {
   if (!label) {
     return null;
   }
@@ -14,7 +16,11 @@ const PillPure: FC<Props> = ({ label, ...props }) => {
   return (
     <SC.Pill>
       <SC.Label>{label}</SC.Label>
-      <SC.ClearButton type='button' {...props}>
+      <SC.ClearButton
+        type='button'
+        onClick={onClear}
+        aria-label={localization.query.reset}
+      >
         <SC.CrossIcon />
       </SC.ClearButton>
     </SC.Pill>
