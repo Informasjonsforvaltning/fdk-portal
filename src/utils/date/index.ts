6ebc85bded946489b231c4ea4bd0ab7d106a8731
Pooria Mehregan <pooriamehregan@gmail.com>
2023-08-29 07:51:59 +0200

    fix(concept): hide empty fields

--- src/utils/date/index.ts
+++ src/utils/date/index.ts
@@ -1,12 +1,21 @@
+/**
+ * Format ISO date string to locale string
+ * @param isoDate ISO date string
+ * @param options available format options
+ *     weekday: 'long',
+ *     year: 'numeric',
+ *     month: 'long',
+ *     day: 'numeric',
+ *     hour: '2-digit',
+ *     minute: '2-digit'
+ * @returns formatted date string
+ */
 export const formatISO = (
   isoDate: string,
   options: Intl.DateTimeFormatOptions = {
-    weekday: 'long',
     year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-    hour: '2-digit',
-    minute: '2-digit'
+    month: 'numeric',
+    day: 'numeric'
   }
 ) => {
   if (!isoDate) {
