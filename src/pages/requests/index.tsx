3f6d26555d9accf73d8a393fa8887a03780ee6d0
Hege Aalvik <hegeaal@gmail.com>
2023-10-03 14:14:17 +0200

    feat: add banner to requests page

--- src/pages/requests/index.tsx
+++ src/pages/requests/index.tsx
@@ -1,4 +1,10 @@
 import React from 'react';
+import Banner from '../../components/banner';
+import localization from '../../lib/localization';
 
-const RequestsPage = () => <main id='content' className='container' />;
+const RequestsPage = () => (
+  <main id='content' className='container'>
+    <Banner title={localization.page.requests} />
+  </main>
+);
 export default RequestsPage;
