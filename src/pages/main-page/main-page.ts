1862f1567839fae6bae147f625508c7059bdedbe
Jeff Reiffers <jeffreiffers@gmail.com>
2024-01-05 09:50:18 +0100

    fix: remove drupal integration

--- src/pages/main-page/main-page.ts
+++ src/pages/main-page/main-page.ts
@@ -4,13 +4,7 @@ import MainPagePure from './main-page-pure';
 import withEntities from '../../components/with-entities';
 import withReferenceData from '../../components/with-reference-data';
 import withCommunity from '../../components/with-community';
-import { mainPageResolver } from './main-page-resolver';
 
-const enhance = compose(
-  mainPageResolver,
-  withEntities,
-  withReferenceData,
-  withCommunity
-);
+const enhance = compose(withEntities, withReferenceData, withCommunity);
 
 export const MainPage = enhance(MainPagePure);
