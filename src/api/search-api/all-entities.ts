16fa340e8aecc55c828ed836af94c94b77072f48
Hege Aalvik <48259307+hegeaal@users.noreply.github.com>
2024-03-18 15:27:48 +0100

    chore: update structure of sorting data and pagination data to work with fdk-search-service (#1912)
    * chore: update structure of sorting data to work with fdk-search-service
    
    * chore: update pagination object to work with fdk-search-service
    
    * chore: rename files and functions from fulltext to search-api

--- src/api/search-api/all-entities.ts
+++ src/api/search-api/all-entities.ts
@@ -1,8 +1,15 @@
-import { searchApiPost, searchApiGet } from './host';
+import { searchApiPost } from './host';
 
 export const searchAllEntities = (body: any) => searchApiPost('', body);
 
-export const getRecentEntities = () => searchApiGet('/recent');
+export const getRecentEntities = () =>
+  searchApiPost('', {
+    pagination: { size: 5, page: 0 },
+    sort: {
+      field: 'FIRST_HARVESTED',
+      direction: 'DESC'
+    }
+  });
 
 export const extractEntities = (searchResponse: any) =>
   searchResponse?.hits ?? [];
