6a6cd19b029be10391016d94d79ce7e0a98f0136
Pooria Mehregan <pooriamehregan@gmail.com>
2024-03-20 08:38:10 +0100

    chore(detail-pages): replace harvester-bff with resource-service

--- src/env.ts
+++ src/env.ts
@@ -13,8 +13,6 @@ export default validateEnv(
     FDK_REGISTRATION_BASE_URI:
       'https://registrering.staging.fellesdatakatalog.digdir.no',
     ADMIN_GUI_BASE_URI: 'https://admin.staging.fellesdatakatalog.digdir.no',
-    INFORMATIONMODEL_HARVESTER_HOST:
-      'https://informationmodels.staging.fellesdatakatalog.digdir.no',
     FDK_LOGGING_FUNCTION_URI:
       'https://europe-west1-digdir-cloud-functions.cloudfunctions.net/cloud-logging',
     FDK_COMMUNITY_BASE_URI:
@@ -26,6 +24,8 @@ export default validateEnv(
     FDK_USER_FEEDBACK_SERVICE_BASE_URI:
       'https://europe-west1-digdir-cloud-functions.cloudfunctions.net/user-feedback-service-staging',
     AI_PROJECT_SERVICE_BASE_URI:
-      'https://ai-project.staging.fellesdatakatalog.digdir.no'
+      'https://ai-project.staging.fellesdatakatalog.digdir.no',
+    RESOURCE_API_HOST:
+      'https://resource.api.staging.fellesdatakatalog.digdir.no'
   }
 );
