ef11eda4c584b4f789e9830ff953e98161d6fe93
hegeaal <hegeaal@gmail.com>
2024-03-21 10:24:28 +0100

    fix: make search calls support new structure

--- src/pages/search-page/search-page-resolver.ts
+++ src/pages/search-page/search-page-resolver.ts
@@ -3,62 +3,35 @@ import { resolve } from 'react-resolver';
 
 import { searchAllEntities } from '../../api/search-api/all-entities';
 import { parseSearchParams } from '../../lib/location-history-helper';
-import { PATHNAME_SEARCH } from '../../constants/constants';
+import { paramsToSearchBody } from '../../utils/common';
 
 const memoizedSearchAllEntities = memoize(searchAllEntities);
-
-const parseAvailabilityFilters = (availability: any, filters: any) => {
-  availability.split(',').forEach((item: any) => {
-    switch (item) {
-      case 'isFree':
-        return filters.push({ isFree: 'true' });
-      case 'isOpenAccess':
-        return filters.push({ isOpenAccess: 'true' });
-      case 'isOpenLicense':
-        return filters.push({ isOpenLicense: 'true' });
-      default:
-        return null;
-    }
-  });
-};
 
 const mapProps = {
   searchAllEntities: ({ location }: any) => {
     const {
       q,
       orgPath,
-      losTheme: los,
+      losTheme,
       page = 0,
       sortfield,
-      accessrights: accessRights,
-      availability,
+      accessrights,
       opendata,
       theme
     } = parseSearchParams(location);
 
-    const filters = [];
-    orgPath && filters.push({ orgPath });
-    los && filters.push({ los });
-    accessRights && filters.push({ accessRights });
-    availability && parseAvailabilityFilters(availability, filters);
-    opendata && filters.push({ opendata: 'true' });
-    theme && filters.push({ theme });
+    const searchAllEntitiesSearchBody = paramsToSearchBody({
+      q,
+      page,
+      orgPath,
+      losTheme,
+      sortfield,
+      accessrights,
+      opendata,
+      theme
+    });
 
-    const searchAllEntitiesParams =
-      location.pathname === PATHNAME_SEARCH
-        ? {
-            q,
-            ...(filters.length > 0 && { filters }),
-            ...(page && {
-              pagination: { page: parseInt(page.toString(), 10) }
-            }),
-            ...(sortfield && {
-              sorting: { field: sortfield, direction: 'DESC' }
-            })
-          }
-        : { q };
-
-    return memoizedSearchAllEntities(searchAllEntitiesParams);
+    return memoizedSearchAllEntities(searchAllEntitiesSearchBody);
   }
 };
 
