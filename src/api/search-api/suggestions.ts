95de50a1e7add049fab79236013d9fe9003b8ba2
Jeff Reiffers <jeffreiffers@gmail.com>
2024-04-09 07:45:04 +0200

    fix: remove double url encoding

--- src/api/search-api/suggestions.ts
+++ src/api/search-api/suggestions.ts
@@ -15,7 +15,7 @@ export const getSearchSuggestions = (
   return searchApiGet(
     searchPath,
     new URLSearchParams({
-      q: encodeURI(q),
+      q,
       ...(!!transport && { profile: 'TRANSPORT' })
     })
   );
