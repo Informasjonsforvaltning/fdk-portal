9a475c5c556064413c4bfaf10022d31f04da49f6
Jeff Reiffers <jeffreiffers@gmail.com>
2024-04-10 13:43:16 +0200

    chore: remove todo

--- src/components/with-concepts/redux/saga.ts
+++ src/components/with-concepts/redux/saga.ts
@@ -19,7 +19,7 @@ function* getConceptsRequested({
   try {
     const data: Record<string, any> = yield call(
       searchConcepts,
-      paramsToSearchBody({ size, filters: { uri } }) // TODO JERE: Add seeAlso to filters
+      paramsToSearchBody({ size, filters: { uri } })
     );
 
     if (data) {
