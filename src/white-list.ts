f3f3e81d58c6721936dda36320e735e8565d8cd7
Pooria Mehregan <pooriamehregan@gmail.com>
2024-05-23 13:45:44 +0200

    feat: add production item to whitelist

--- src/white-list.ts
+++ src/white-list.ts
@@ -1,6 +1,13 @@
+/**
+ * This file contains a list of resources that can have a button to let the user request the data.
+ * The requestAdress field is the application form adress for requestsing access to the data,
+ * and the id is the id of the specific resource.
+ */
+
 import { AccessRequest } from './types';
 
 export const accessRequestWhiteList: AccessRequest[] = [
+  // Staging
   {
     id: '788dc86c-7e14-3a52-8f9e-2808001fbf44',
     requestAddress: 'http://kyv.apps.tt02.altinn.no/kyv/tilgangsoknad-pilot'
@@ -8,5 +15,10 @@ export const accessRequestWhiteList: AccessRequest[] = [
   {
     id: '8f305377-a3a3-3dde-be33-d5b5d0b8f818',
     requestAddress: 'https://forms.gle/VnBnZwvsWSu51a3V6'
+  },
+  // Production
+  {
+    id: 'a49ddd4a-8ccf-3054-8164-0bb9bfc9783c',
+    requestAddress: 'https://kyv.apps.altinn.no/kyv/tilgangsoknad-pilot'
   }
 ];
