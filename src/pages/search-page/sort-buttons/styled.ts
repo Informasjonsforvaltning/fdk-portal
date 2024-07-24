2b6447cebe2614dd15d55f37be2db83ee05a2725
hegeaal <hegeaal@gmail.com>
2024-07-24 10:32:13 +0200

    feat: update sort buttons design

--- src/pages/search-page/sort-buttons/styled.ts
+++ src/pages/search-page/sort-buttons/styled.ts
@@ -4,6 +4,8 @@ import ButtonToggleSC from '../../../components/button-toggle/styled';
 
 const SortButtons = styled.div`
   display: flex;
+  padding-top: 2rem;
+  padding-bottom: 2rem;
   justify-content: center;
   @media (min-width: 992px) {
     justify-content: flex-end;
