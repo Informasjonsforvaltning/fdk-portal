a821eed8dfc8f25005121a0bb1820d12d5195174
halvor <halvorbmundal@gmail.com>
2024-09-19 12:53:59 +0200

    Replace white list requestAddress for Samferdselsdata in staging

--- src/white-list.ts
+++ src/white-list.ts
@@ -10,7 +10,7 @@ export const accessRequestWhiteList: AccessRequest[] = [
   // Staging
   {
     id: '788dc86c-7e14-3a52-8f9e-2808001fbf44',
-    requestAddress: 'http://kyv.apps.tt02.altinn.no/kyv/tilgangsoknad-pilot'
+    requestAddress: 'https://digdir.apps.tt02.altinn.no/digdir/tilgangssoknad/'
   },
   {
     id: '8f305377-a3a3-3dde-be33-d5b5d0b8f818',
